e4df3f121180a1f785f469dbc8aee3893e3151b6
Vladislav <barabashkavq2000@gmail.com>
2020-10-01 17:56:39 +0300

    feat: add functions: max ,avg min

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
+let array =  [1, 2, 3, 4];
 
 exports.min = function min (array) {
-  return 0;
+    if (array) {
+        if (typeof array !== 'undefined' && array.length > 0 && Array.isArray(array)) {
+            let min = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (min > array[i]) min = array[i];
+            }
+            return min;
+
+        } else {
+            return 0;
+        }
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array) {
+        if (typeof array !== 'undefined' && array.length > 0 && Array.isArray(array)) {
+            let max = array[0];
+            for (let i = 0; i < array.length; i++) {
+                if (max < array[i]) max = array[i];
+            }
+            return max;
+        } else {
+            return 0;
+        }
+    }
 }
+    exports.avg = function avg(array) {
+        if (array) {
+            if (typeof array !== 'undefined' && array.length > 0 && Array.isArray(array)) {
+                let total = 0;
+                let avg;
+                for (let i = 0; i < array.length; i++) {
+                    total += array[i];
+                    avg = total / array.length;
+                }
+                return avg;
+            } else {
+                return 0;
+            }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+        }
+    }
